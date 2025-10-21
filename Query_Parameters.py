f9b3b0aac85b26b50494fa1a43e3706db3135cd1
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-10-21 22:28:14 +0600

    Fast api practice

---
+++ Query_Parameters.py
@@ -0,0 +1,29 @@
+# Query Parameters(default)
+from fastapi import FastAPI
+app=FastAPI()
+
+@app.get("/blog")
+def show_comment(limit=10,published:bool=True):
+    if published:
+       return {f"{limit} blog was published"}
+    else:
+        return{"Message:"f"{limit} blog was not published"}
+
+
+# optional parameters (pizza order)
+
+@app.get("/pizza/{pizza_id}")
+def order_pizza(pizza_id:int,topping1:str|None=None,topping2:str|None=None):
+    pizza={"pizza_id":pizza_id,"base":"cheese"}
+    toppings=[]
+    if topping1:
+        toppings.append(topping1)
+    if topping2:
+        toppings.append(topping2)
+
+    if toppings:
+        pizza["toppings"]=toppings
+    else:
+        pizza["toppings"]=["Default topping:tomato sauce"]
+    return pizza
+
