5a152068adad32abbc02f32369cd246ed50b068d
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-11-07 22:52:44 +0600

    python request body code

--- Request_Body.py
+++ Request_Body.py
@@ -1,24 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+app =FastAPI()
 
 class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-
-app = FastAPI()
-
+    name:str
+    description:str|None=None
+    price:float
+    tax:float|None=None
 
 @app.post("/items/")
-async def create_item(item: Item):
+def create(item:Item):
     return item
-
-
-
-
-
-
-
