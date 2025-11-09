2779ac920846ffc77254a07fd199f26612940cd8
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-11-09 23:13:09 +0600

    python request body

--- Request_Body.py
+++ Request_Body.py
@@ -20,3 +20,12 @@ def create(item:Item):
 @app.put("/items/{item_id}")
 def update(item_id:str, item:Item):
     return {"item_id":item_id,"item":item}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
