9bceb0b94d5742c7c9357cd3887e84e516881f79
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-11-12 22:25:10 +0600

    python query parameters

--- Query_Parameters.py
+++ Query_Parameters.py
@@ -40,3 +40,11 @@ def order_pizza(pizza_id:int,topping1:str):
 async def read_user_item(item_id: str, needy: str):
     item = {"item_id": item_id, "needy": needy}
     return item
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
