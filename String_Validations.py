8785a33f4007f8d46d9746976bb5bdace1d9d066
Md Taofick <107477685+taofick221@users.noreply.github.com>
2025-11-16 22:46:26 +0600

    python string validation

--- String_Validations.py
+++ String_Validations.py
@@ -9,3 +9,18 @@ async def read_items(q: str | None = None):
     if q:
         results.update({"q": q})
     return results
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None, Query(max_length=50)] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
